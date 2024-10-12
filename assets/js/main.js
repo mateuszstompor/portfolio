d628d199f54a692e5056d63f5f0a6c04092234aa
Mateusz Stompór <mateusz.stompor@gmail.com>
2024-10-12 22:56:58 +0200

    Add experience section

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,7 +58,7 @@ function sendMail() {
     window.location.href = mailtoLink;
 }
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text, .experience__subtitle, .experience__text',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input, .button__download',{interval: 200}); 
