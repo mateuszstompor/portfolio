a6a5481bc2e3224a01b911c1208264d346959738
Mateusz Stompór <mateusz.stompor@gmail.com>
2024-12-01 20:01:36 +0100

    Fix placeholders

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,4 +61,4 @@ function sendMail() {
 sr.reveal('.home__data, .about__img, .skills__subtitle, .work__subtitle, .work__text, .work__img_subtitle, .skills__text, .experience__subtitle, .experience__text',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input, .button__download',{interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input, .visually-hidden, .button__download',{interval: 200}); 
